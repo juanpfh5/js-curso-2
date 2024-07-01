bb5d30f7678e6c54b1f439e4b212e0561ebe1285
Juan Pablo F. H. <20206470@garzas.uatx.mx>
2024-07-01 06:15:42 -0600

    Elaboración del desafío 1.9
    Lógica de programación: explorar funciones y listas (Alura)

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+let titulos = document.querySelector('h1');
 
+titulos.innerHTML = 'Hora del Desafío';
+
+function botonClicado() {
+    alert('El botón fue clicado');
+}
+
+function nombreCiudadBrasil() {
+    let ciudad = prompt('Ingrese el nombre de una ciudad de Brasil: ');
+    alert(`Estuve en ${ciudad} y me acordé de tí.`);
+}
+
+function alertaAmoJS() {
+    alert('Yo amo JavaScript');
+}
+
+function sumaNumeros() {
+    let numero1 = parseInt(prompt('Ingrese un número: '));
+    let numero2 = parseInt(prompt('Ingrese otro número: '));
+    let suma = numero1 + numero2;
+    alert(`La suma de los números es: ${suma}`);
+}
